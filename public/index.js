8e2d6fa037bb1a3b57f99a86fe41f21a96beddbd
ipincamp <arif.lngsrn@gmail.com>
2022-02-09 18:24:51 +0700

    Showing owner video

---
+++ public/index.js
@@ -0,0 +1,69 @@
+/**
+ * @name yt2mp3
+ * @version v1.0.1
+ * @author ipincamp <support@nur-arifin.my.id>
+ * @license GNU (General Public License v3.0)
+ */
+
+const cvButton = document.querySelector('.cvb');
+const inputURL = document.querySelector('.url');
+const thumbURL = document.querySelector('.tmb');
+const videoTtl = document.querySelector('.ttl');
+const videoOwn = document.querySelector('.own');
+const downBttn = document.querySelector('.dwn');
+
+const getVideoID = () => {
+  const url = inputURL.value;
+
+  if (url.includes('youtu.be')) {
+    return url.slice(-11);
+  }
+  const searchParams = new URLSearchParams(url.split('?')[1]);
+
+  return searchParams.get('v');
+};
+
+const getVideoInfo = async (id) => {
+  const res = await fetch(`/api?id=${id}`);
+
+  return res.json();
+};
+
+const getFormats = (name) => document.querySelector(`[name="${name}"]:checked`).value;
+
+const getDownloadAnchor = ({ id, format }) => {
+  let url = `/convert?id=${id}&format=${format}`;
+
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = true;
+
+  return a;
+};
+
+const download = ({ id, format }) => {
+  const a = getDownloadAnchor({ id, format });
+  a.click();
+};
+
+cvButton.addEventListener(
+  'click',
+  async () => {
+    const id = getVideoID();
+    const { title, owner, thumbnail } = await getVideoInfo(id);
+
+    videoTtl.textContent = title;
+    videoOwn.textContent = `By ${owner}`;
+    thumbURL.src = thumbnail;
+  },
+);
+
+downBttn.addEventListener(
+  'click',
+  () => {
+    download({
+      id: getVideoID(),
+      format: getFormats('format'),
+    });
+  },
+);
