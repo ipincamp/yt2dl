bb9602ba686d149e33b5fe7bafeb06b186725686
ipincamp <arif.lngsrn@gmail.com>
2022-02-17 07:48:55 +0700

    Create playlist API
    1. If detected id is part of playlist, it will give playlist as results.
    2. Running with async/await
    3. Given result for playlist with array as id, object for each videos.

--- src/events/api.js
+++ src/events/api.js
@@ -1,46 +1,80 @@
 /**
  * @name yt2mp3
- * @version v1.0.1
+ * @version v1.0.2
  * @author ipincamp <support@nur-arifin.my.id>
  * @license GNU (General Public License v3.0)
  */
 
 const ytdl = require('ytdl-core');
+const ytpl = require('ytpl');
 const { Joi, validate } = require('express-validation');
 const { last } = require('lodash');
 
 module.exports = {
   name: '/api',
-  run(req, res, next) {
+  async run(req, res, next) {
     validate({
       query: Joi.object({
         id: Joi.string().required(),
       }),
     });
     const { id } = req.query;
+    try {
+      await ytdl
+        .getInfo(id)
+        .then(({ videoDetails }) => {
+          const {
+            title, thumbnails, ownerChannelName, publishDate,
+          } = videoDetails;
+          const thumbnail = last(thumbnails).url;
+          const owner = ownerChannelName;
 
-    ytdl
-      .getInfo(id)
-      .then(({ videoDetails }) => {
-        const {
-          title, thumbnails, ownerChannelName, publishDate,
-        } = videoDetails;
-        const thumbnail = last(thumbnails).url;
-        const owner = ownerChannelName;
+          function invertDate(str) {
+            return str.split('-').reverse().join('/');
+          }
 
-        function invertDate(str) {
-          return str.split('-').reverse().join('/');
-        }
+          const uploadDate = invertDate(publishDate);
 
-        const uploadDate = invertDate(publishDate);
-
-        res.json({
-          title,
-          owner,
-          uploadDate,
-          thumbnail,
+          res.json({
+            title,
+            owner,
+            uploadDate,
+            thumbnail,
+          });
         });
-      })
-      .catch((err) => next(err));
+    } catch {
+      await ytpl(id)
+        .then((details) => {
+          let {
+            author,
+            estimatedItemCount,
+            items,
+            title,
+            thumbnails,
+            url,
+          } = details;
+
+          const owner = author.name;
+          const thumbnail = last(thumbnails).url;
+          let videos = [];
+
+          items.forEach((v) => {
+            videos.push(v.id, {
+              owner: v.author.name,
+              title: v.title,
+              thumbnail: last(v.thumbnails).url,
+            });
+          });
+
+          res.json({
+            title,
+            owner,
+            url,
+            estimatedItemCount,
+            thumbnail,
+            videos,
+          });
+        }).catch((err) => next(err));
+    }
   },
 };
