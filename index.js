9ab4293fa018b8c4ad11379673e07ce6b1dd41d7
ipincamp <arif.lngsrn@gmail.com>
2022-02-18 02:03:57 +0700

    Bump v1.0.2
    Generate random port if not defined. Will be show every starting.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  * @name yt2mp3
- * @version v1.0.1
+ * @version v1.0.2
  * @author ipincamp <support@nur-arifin.my.id>
  * @license GNU (General Public License v3.0)
  */
@@ -11,7 +11,7 @@ const { join } = require('path');
 const { fetchAPI } = require('./src/fetchAPI');
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || Math.floor(Math.random() * (9999 - 6000 + 1)) + 6000;
 app.use(express.static('public'));
 
 const eventFiles = fs.readdirSync(join(__dirname, './src/events'))
@@ -28,4 +28,4 @@ setInterval(() => {
   fetchAPI(url);
 }, 299000);
 
-app.listen(port);
+app.listen(port, () => console.info(`Listening server at port ${port}`));
