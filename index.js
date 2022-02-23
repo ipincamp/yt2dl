e408db1fb38ec1e425d0bde25094be0c6397e896
ipincamp <arif.lngsrn@gmail.com>
2022-02-23 10:22:48 +0700

    Update some index

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  */
 
 const express = require('express');
-const fs = require('fs');
 const { join } = require('path');
+const { readdirSync } = require('fs');
 const { fetchAPI } = require('./src/fetchAPI');
 
 const app = express();
 const port = process.env.PORT || Math.floor(Math.random() * (50000 - 10000 + 1)) + 10000;
+const url = 'https://y2m.herokuapp.com/';
+
 app.use(express.static('public'));
 
-const eventFiles = fs.readdirSync(join(__dirname, './src/events'))
+setInterval(() => {
+  fetchAPI(url);
+}, 299000);
+
+const eventFiles = readdirSync(join(__dirname, './src/events'))
   .filter((x) => x.endsWith('.js'));
 for (const file of eventFiles) {
   const event = require(join(__dirname, './src/events', `${file}`));
 
   app.get(event.name, (...args) => event.run(...args));
 }
-
-const url = 'https://y2v.herokuapp.com/';
-
-setInterval(() => {
-  fetchAPI(url);
-}, 299000);
 
 app.listen(port, () => console.info(`Listening server at port ${port}`));
