2c08eb66f06d09c49313b82bc37f5f145718dfa1
ipincamp <arif.lngsrn@gmail.com>
2022-04-20 22:01:10 +0700

    Major update
    - Fix ID Filtering
    - Modification of params
    - Validate ID
    - Minimalist json response API
    - Add more parameter for audio bitrate
    - Handles almost 90% of unexpected errors

--- index.js
+++ index.js
@@ -15,61 +15,52 @@ import sanitize from 'sanitize-filename';
 import ytdlcore from 'ytdl-core';
 
 const app = expres();
-const { forEach, last } = lodash;
+const { chain, forEach, last } = lodash;
 const { Joi, validate } = valids;
 const { spawn } = cp;
 
-function getId(url) {
-  function onlyId(s) {
-    const a = url.split(s)[1].slice(0, 11);
-    if (a.length !== 11) {
-      return null;
-    }
-    return a;
-  }
+/**
+ *
+ * @param {string} url - YouTube URL
+ * @returns {string | null}
+ */
+const ID = (url) => {
   if (url.length === 11) {
     return url;
   }
   if (url.includes('youtu.be')) {
-    onlyId('be/');
-  }
-  if (url.includes('youtube.com/watch')) {
-    onlyId('?v=');
+    const id = url.split('be/')[1].slice(0, 11);
+    if (id.length === 11) {
+      return id;
+    }
+    return null;
   }
   if (url.includes('youtube.com/shorts')) {
-    onlyId('ts/');
+    const id = url.split('ts/')[1].slice(0, 11);
+    if (id.length === 11) {
+      return id;
+    }
+    return null;
+  }
+  if (url.includes('youtube.com/watch')) {
+    const id = url.split('?v=')[1].slice(0, 11);
+    if (id.length === 11) {
+      return id;
+    }
+    return null;
   }
   return null;
-}
-
-function getQuality(formats, select) {
-  const res = {
-    fps: 0,
-    height: 0,
-    heights: [],
-    itag: 0,
-  };
-  if (select !== undefined) {
-    res.fps = parseInt(select.split('p')[1]);
-    res.height = parseInt(select.split('p')[0]);
-  }
-  formats.forEach((f) => {
-    if (
-      f.hasAudio === false
-      && f.hasVideo === true
-      && f.videoCodec?.startsWith('avc1')
-    ) {
-      res.heights.push(`${f.height}p${f.fps}`);
-      if (res.fps === f.fps && res.height === f.height) {
-        res.itag = f.itag;
-      }
-    }
-  });
-  return res;
-}
+};
 
 app.use(cors());
 app.get('/', (req, res) => res.sendStatus(200));
+/**
+ * Parameter example
+ * - /api?url=12345678901
+ * - /api?url=https://youtu.be/12345678901
+ * - /api?url=https://www.youtube.com/watch?v=12345678901
+ * - /api?url=https://www.youtube.com/shorts/12345678901
+ */
 app.get(
   '/api',
   validate({
@@ -77,166 +68,166 @@ app.get(
   }),
   (req, res, next) => {
     const { url } = req.query;
-    if (getId(url) === null) {
-      res.status(400).json({
-        status: false,
-        input: url,
-        error: 'invalid input',
-      });
-      return;
-    }
-    ytdlcore
-      .getInfo(getId(url))
-      .then(({ videoDetails, formats }) => {
-        res.status(200).json({
-          status: true,
-          input: url,
-          results: {
-            url: videoDetails.video_url,
+    if (ID(url) === null) return res.sendStatus(400);
+    if (ytdlcore.validateID(ID(url)) === true) {
+      ytdlcore
+        .getInfo(ID(url))
+        .then(({ formats, videoDetails }) => {
+          const resolutions = formats
+            .filter((f) => f.hasAudio === false && f.hasVideo === true && f.videoCodec?.startsWith('avc1'))
+            .map(({ fps, height }) => `${height}p${fps}`);
+          const bitrate = chain(formats)
+            .filter('hasAudio')
+            .map('audioBitrate')
+            .uniq()
+            .orderBy(null, 'desc')
+            .value();
+          res.status(200).json({
             title: videoDetails.title,
-            description: videoDetails.description,
             thumbnail: last(videoDetails.thumbnails).url,
             duration: parseInt(videoDetails.lengthSeconds),
-            category: videoDetails.category,
-            publishdate: videoDetails.publishDate,
-            likes: parseInt(videoDetails.likes),
-            dislikes: parseInt(videoDetails.dislikes),
-            views: parseInt(videoDetails.viewCount),
-            private: videoDetails.isPrivate,
-            channelname: videoDetails.ownerChannelName,
-            channelurl: videoDetails.ownerProfileUrl,
-            resolutions: getQuality(formats).heights,
-          },
-        });
-      })
-      .catch((error) => next(error));
+            channel: videoDetails.ownerChannelName,
+            resolutions,
+            audiobitrate: bitrate,
+          });
+        })
+        .catch((error) => next(error.message));
+    } else {
+      return res.sendStatus(404);
+    }
   },
 );
+/**
+ * Parameter examples
+ * - Audio: /get?url=12345678901&typ=aud&bit=128
+ * - Video: /get?url=12345678901&typ=vid&qty=1080p60&bit=128
+ */
 app.get(
   '/get',
   validate({
     query: Joi.object({
       url: Joi.string().required(),
-      type: Joi.valid('video', 'audio'),
-      reso: Joi.when('type', {
-        is: Joi.valid('video'),
+      typ: Joi.valid('aud', 'vid'),
+      bit: Joi.when('typ', {
+        is: Joi.valid('aud', 'vid'),
+        then: Joi.number().required(),
+      }),
+      qty: Joi.when('typ', {
+        is: Joi.valid('vid'),
         then: Joi.string().required(),
       }),
     }),
   }),
   (req, res, next) => {
-    const { url, type } = req.query;
-    if (getId(url) === null || !req.query.reso.includes('p')) {
-      res.status(400).json({
-        status: false,
-        input: url,
-        error: 'invalid input',
-      });
-      return;
-    }
-    ytdlcore
-      .getInfo(getId(url))
-      .then(({ formats, videoDetails }) => {
-        const { title } = videoDetails;
-        const streams = {};
-        if (type === 'audio') {
-          streams.audio = ytdlcore(getId(url), { quality: 'highestaudio' });
-        }
-        if (type === 'video') {
-          const { itag } = getQuality(formats, req.query.reso);
-          if (itag === 0) {
-            return res.status(404).json({
-              status: false,
-              input: url,
-              error: 'invalid resolution',
+    const { url, typ, qty } = req.query;
+    if (ID(url) === null) return res.sendStatus(400);
+    if (ytdlcore.validateID(ID(url)) === true) {
+      ytdlcore
+        .getInfo(ID(url))
+        .then(({ formats, videoDetails }) => {
+          const { title } = videoDetails;
+          let audioItag;
+          let sampleBit;
+          let videoItag;
+          let ffmpegLog = '';
+          let queryFill = `${qty}_${req.query.bit}k`;
+          const streams = {};
+          if (typ === 'aud') {
+            formats.forEach((f) => {
+              if (f.audioBitrate === parseInt(req.query.bit)) {
+                audioItag = f.itag;
+                sampleBit = f.audioSampleRate;
+              }
             });
+            if (audioItag === undefined) return res.sendStatus(400);
+            streams.aud = ytdlcore(ID(url), { quality: audioItag });
           }
-          streams.video = ytdlcore(getId(url), { quality: itag });
-          streams.audio = ytdlcore(getId(url), { quality: 'highestaudio' });
-        }
-        const exts = { video: 'mp4', audio: 'mp3' };
-        const contentTypes = { video: 'video/mp4', audio: 'audio/mpeg' };
-        const ext = exts[type];
-        const contentType = contentTypes[type];
-        const name = `${encodeURI(sanitize(title))}-${req.query.reso}.${ext}`;
-        res.setHeader('Content-Type', contentType);
-        res.setHeader(
-          'Content-Disposition',
-          `attachment; filename=${name}; filename*=utf-8''${name}`,
-        );
-        const pipes = {
-          out: 1,
-          err: 2,
-          video: 3,
-          audio: 4,
-        };
-        const ffmpegInputOptions = {
-          video: [
-            '-i',
-            `pipe:${pipes.video}`,
-            '-i',
-            `pipe:${pipes.audio}`,
-            '-map',
-            '0:v',
-            '-map',
-            '1:a',
-            '-c:v',
-            'copy',
-            '-c:a',
-            'libmp3lame',
-            '-crf',
-            '27',
-            '-preset',
-            'veryfast',
-            '-movflags',
-            'frag_keyframe+empty_moov',
-            '-f',
-            'mp4',
-          ],
-          audio: [
-            '-i',
-            `pipe:${pipes.audio}`,
-            '-c:a',
-            'libmp3lame',
-            '-vn',
-            '-ar',
-            '44100',
-            '-ac',
-            '2',
-            '-b:a',
-            '192k',
-            '-f',
-            'mp3',
-          ],
-        };
-        const ffmpegOptions = [
-          ...ffmpegInputOptions[type],
-          '-loglevel',
-          'error',
-          '-',
-        ];
-        const ffmpegProcess = spawn(ffmpeg, ffmpegOptions, {
-          stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe'],
-        });
-        const ffmpegStreamError = (e) => console.error(e);
-        forEach(streams, (stream, format) => {
-          const dest = ffmpegProcess.stdio[pipes[format]];
-          stream.pipe(dest).on('error', ffmpegStreamError);
-        });
-        let logs = '';
-        ffmpegProcess.stdio[pipes.out].pipe(res);
-        ffmpegProcess.stdio[pipes.err].on(
-          'data',
-          (chunk) => (logs += chunk.toString()),
-        );
-        ffmpegProcess.on('exit', (exitCode) => {
-          if (exitCode === 1) {
-            console.info(logs);
+          if (typ === 'vid') {
+            const r = qty?.split('p');
+            videoItag = chain(formats)
+              .filter(({ fps, height }) => fps === parseInt(r[1]) && height === parseInt(r[0]))
+              .map('itag')
+              .orderBy(null, 'desc')
+              .head()
+              .value();
+            if ((videoItag || audioItag) === undefined) return res.sendStatus(400);
+            streams.aud = ytdlcore(ID(url), { quality: audioItag });
+            streams.vid = ytdlcore(ID(url), { quality: videoItag });
           }
-          res.end();
-        });
-      })
-      .catch((error) => next(error));
+          const exts = {
+            aud: 'mp3',
+            vid: 'mp4',
+          };
+          const contentTypes = {
+            aud: 'audio/mpeg',
+            vid: 'video/mp4',
+          };
+          if (typ === 'aud') {
+            queryFill = `${req.query.bit}k`;
+          }
+          const f = `${encodeURI(sanitize(title))}-[${queryFill}].${exts[typ]}`;
+          res.setHeader('Content-Type', contentTypes[typ]);
+          res.setHeader(
+            'Content-Disposition',
+            `attachment; filename=${f}; filename*=utf-8''${f}`,
+          );
+          const pipes = {
+            out: 1,
+            err: 2,
+            aud: 3,
+            vid: 4,
+          };
+          const ffmpegInputOptions = {
+            aud: [
+              '-i', `pipe:${pipes.aud}`,
+              '-c:a', 'libmp3lame',
+              '-vn',
+              '-ar', `${sampleBit}`,
+              '-ac', '2',
+              '-b:a', `${req.query.bit}k`,
+              '-f', 'mp3',
+            ],
+            vid: [
+              '-i', `pipe:${pipes.vid}`,
+              '-i', `pipe:${pipes.aud}`,
+              '-map', '0:v',
+              '-map', '1:a',
+              '-c:v', 'copy',
+              '-c:a', 'libmp3lame',
+              '-crf', '27',
+              '-preset', 'veryfast',
+              '-movflags', 'frag_keyframe+empty_moov',
+              '-f', 'mp4',
+            ],
+          };
+          const ffmpegOptions = [
+            ...ffmpegInputOptions[typ],
+            '-loglevel', 'error', '-',
+          ];
+          const ffmpegProcess = spawn(ffmpeg, ffmpegOptions, {
+            stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe'],
+          });
+          forEach(streams, (stream, format) => {
+            const dest = ffmpegProcess.stdio[pipes[format]];
+            stream.pipe(dest).on('error', (e) => next(e.message));
+          });
+          ffmpegProcess.stdio[pipes.out].pipe(res);
+          ffmpegProcess.stdio[pipes.err].on(
+            'data',
+            (chunk) => ffmpegLog += chunk.toString(),
+          );
+          ffmpegProcess.on('exit', (exitCode) => {
+            if (exitCode === 1) {
+              next(ffmpegLog);
+            }
+            res.end();
+          });
+          res.on('close', () => ffmpegProcess.kill());
+        })
+        .catch((error) => next(error.message));
+    } else {
+      return res.sendStatus(404);
+    }
   },
 );
 app.listen(process.env.PORT || 8000);
