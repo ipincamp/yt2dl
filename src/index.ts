846ae758a6577a4b7d61eb3797a9f1c098253f6a
ipincamp <support@nur-arifin.my.id>
2025-11-19 01:50:07 +0700

    refactor: Simplify main function structure and remove unused video info retrieval

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,13 @@
-import { Innertube, UniversalCache } from 'youtubei.js';
+/**
+ * @name yt2dl
+ * @version 2.0.0
+ * @author ipincamp <support@nur-arifin.my.id>
+ * @license GPL-3.0 (General Public License v3.0)
+ */
 
-const yt = await Innertube.create({
-  cache: new UniversalCache(true),
-  user_agent:
-    'Mozilla/5.0 (X11; Linux x86_64; rv:145.0) Gecko/20100101 Firefox/145.0',
-  timezone: 'Asia/Jakarta',
-});
-
-async function getVideoInfo(videoId: string) {
-  const videoInfo = await yt.actions.execute('/player', {
-    // You can add any additional payloads here, and they'll merge with the default payload sent to InnerTube.
-    videoId,
-    client: 'YTMUSIC', // InnerTube client to use.
-    parse: true, // tells YouTube.js to parse the response (not sent to InnerTube).
-  });
-
-  return videoInfo;
+async function main() {
+  // Step 1: Provide video ID
+  // Step 2: Fetch video informations
+  // Step 3: Consudme details for your own implementation
 }
-
-// https://www.youtube.com/shorts/XJRH0rT87W0
-const videoInfo = await getVideoInfo('XJRH0rT87W0');
-console.info(videoInfo);
+main();
